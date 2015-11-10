820e4bbf10028c5711244bc29f134da54985b9b3
Dane Schoonover <Dane@Danes-MacBook-Air-2.local>
2015-11-09 18:22:18 -0800

    update

--- week-6/script1.js
+++ week-6/script1.js
@@ -4,30 +4,24 @@ var openWeatherAPIKey = "&APPID=9a15b6a462b0ea3645959ca4a52157f3";
 
 // Returns either the zip code or city input from user
 function getWeatherAPIParameter(){
-    var zip = document.getElementsByName('zip');
-    var city = document.getElementsByName('city');
-    console.log(zip[0].value);
-    console.log(city[0].value);
-    if (zip[0].value)
-        return "http://api.openweathermap.org/data/2.5/weather?zip=" + zip[0].value;
-    else
-        return "http://api.openweathermap.org/data/2.5/weather?q=city" + city[0].value;
+    var loc = document.getElementById('loc').value;
+    if (isNaN(loc)) {
+        return "http://api.openweathermap.org/data/2.5/weather?q=city" + loc + "&units=imperial";
+    }
+    else {
+        return "http://api.openweathermap.org/data/2.5/weather?zip=" + loc  + "&units=imperial";
+    }
+
 }
 
 // Appends weather results with data from http request/response
 function appendWeatherResults(oCity, oTemp, oHumidity){
-    (function(){
-        for(var i = 0; i < 3; i++)
-            document.body.appendChild(document.createElement('paragraph'));
-    })();
-
-    var paragraphs = document.getElementsByTagName('paragraph');
-    var city = document.createTextNode('City: ' + oCity);
-    var temp = document.createTextNode('Temp: ' + oTemp);
-    var humi = document.createTextNode('Humidity: ' + oHumidity);
-    paragraphs[0].appendChild(city);
-    paragraphs[1].appendChild(temp);
-    paragraphs[2].appendChild(humi);
+    var city = document.getElementById("city");
+    var temp = document.getElementById("temp");
+    var humi = document.getElementById("humi");
+    city.textContent = "City: " + oCity;
+    temp.textContent = "Temp: " + oTemp;
+    humi.textContent = "Humidity: " + oHumidity;
 }
 
 // Asynch HTTP request
