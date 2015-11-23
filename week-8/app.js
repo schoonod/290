ce1ffd667f564c0038438bc2a66408378c0ba307
Dane Schoonover <Dane@Danes-iMac.local>
2015-11-22 18:52:06 -0800

    update

--- week-8/app.js
+++ week-8/app.js
@@ -3,14 +3,20 @@ var express = require('express');
 var session = require('express-session');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});  // use 'main' and the default layout
 var bodyParser = require('body-parser');
+var request = require('request');
+
 var app = express();
-app.use(session({secret:'SuperSecretPassword'}));   // secret is an options object
+app.use(session({
+    secret:'SuperSecretPassword',
+    saveUninitialized: true,
+    resave: true
+    }));   // secret is an options object
 app.engine('handlebars', handlebars.engine);        // uses handlebars as the .handlebars file handler
 app.set('view engine', 'handlebars');               // allows omission of .handlebars ext when making file calls
 app.set('port', 3000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(express.static('public'));                  // look for static files in week-8/public
 
 
 // -----Controllers----- //
@@ -18,47 +24,58 @@ app.get('/',function(req,res,next){
     var context = {};
     //If there is no session, go to the main page.
     if(!req.session.name){
+        context.DNE = "Your session does not exist, let's make that for you!";
         res.render('newSession', context);
         return;
     }
+
+    // A session exists and that session can be used to render a view with its information, etc.
     context.name = req.session.name;
-    context.toDoCount = req.session.toDo.length || 0;
-    context.toDo = req.session.toDo || [];
-    console.log(context.toDo);
-    res.render('toDo',context);
+    context.toVisitCount = req.session.toVisit.length || 0;
+    context.toVisit = req.session.toVisit || [];
+    console.log(context.toVisit);
+    res.render('toVisit',context);
 });
 
 app.post('/',function(req,res){
     var context = {};
 
+    // A new session would render toVisit.handlebars, which would return a body
+    // parameter of 'New List' after the user submits form
     if(req.body['New List']){
-        req.session.name = req.body.name;
-        req.session.toDo = [];
+        req.session.name = req.body.username;
+        req.session.toVisit = [];
         req.session.curId = 0;
     }
 
-    //If there is no session, go to the main page.
+    // Catches user if they submit an empty submit box and no session exists
     if(!req.session.name){
+        context.DNE = "Please enter your name.";
         res.render('newSession', context);
         return;
     }
 
-    if(req.body['Add Item']){
-        req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+
+    // If they click the 'Add Location' button
+    // MAPS API probably goes here
+    if(req.body['Add Location']){
+        req.session.toVisit.push({"name":req.body.name, "id":req.session.curId});
         req.session.curId++;
     }
 
-    if(req.body['Done']){
-        req.session.toDo = req.session.toDo.filter(function(e){
+    // If they click the 'Mark as Visited' button
+    if(req.body['Visited']){
+        req.session.toVisit = req.session.toVisit.filter(function(e){
             return e.id != req.body.id;
         })
     }
 
+    // If there is a session but the box is empty?
     context.name = req.session.name;
-    context.toDoCount = req.session.toDo.length;
-    context.toDo = req.session.toDo;
-    console.log(context.toDo);
-    res.render('toDo',context);
+    context.toVisitCount = req.session.toVisit.length;
+    context.toVisit = req.session.toVisit;
+    console.log(context.toVisit);
+    res.render('toVisit',context);
 });
 
 // Page not found
@@ -67,7 +84,7 @@ app.use(function(req,res){
     res.render('404');
 });
 
-// Error handling
+// Error handling; can call this via http requests with next(err)
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('plain/text');
